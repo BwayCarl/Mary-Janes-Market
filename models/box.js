5f7faab453b6fc7db4a7a70ee1b3082cebc13443
Brian GEARTY <geartybrian@gmail.com>
2020-11-19 21:38:58 -0500

    added server.js to connect to database.. added a models folder with box.js and customer.js..made an images folder to hold pics for boxes.

---
+++ models/box.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BoxSchema = new Schema ({
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        img_url: {
+            type: String,
+            required: true
+        },
+        boolean: Boolean,
+
+        array: Array,
+
+        date: {
+            type: Date,
+            default: Date.now
+        },
+    });
+
+const Box = mongoose.model("Box", BoxSchema);
+
+module.exports = Box;
---
+++ models/customer.js
@@ -0,0 +1,21 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const CustomerSchema = new Schema({
+//         Whole_name: {
+//             type: String,
+//             required: true
+//         },
+//         Email: {
+//             type: String,
+//             required: true,
+//         },
+//         DOB: {
+//             type: String,
+//             required: true,
+//         },
+//     });
+
+//     const Customer = mongoose.model("Customer", CustomerSchema);
+
+//     module.exports = Customer;
