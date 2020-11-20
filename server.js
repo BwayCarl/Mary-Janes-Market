af5cb417a4b0b265ecb0d8ca9b18f1e421c9f77c
Brian GEARTY <geartybrian@gmail.com>
2020-11-20 08:45:11 -0500

    put the DB seed in a seedDB.js file in SRC folder. Added "morgan" logger to package.json got api/boxes working in server.js

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
 const mongoose = require("mongoose");
 const Box = require("./models/box.js");
+const express = require("express");
+const logger = require("morgan");
 
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/BoxSet", { useNewUrlParser: true });
 
-boxes = [
-    {
-        Name: "Stoner Box",
-        Price: 100,
-        Category: "Box Sets",
-        Img_url: "Image/GoodWeed.png"
-    },
-    {
-        Name:"Stressed-Out Pro Box",
-        Price: 75,
-        Category: "Box Sets",
-        Img_url: "Image/BadWeed.png"
-    },
-    {
-        Name: "Creative Box",
-        Price: 150,
-        Category: "Box Sets",
-        Img_url: "Image/UncleSkunker.png"
-    }
-]
 
-Box.collection.insertMany(boxes)
-.then(BoxSet => {
-    console.log(BoxSet);
-})
-.catch(({ message}) => {
-    console.log(message)
+app.get("/api/boxes", (req, res) => {
+    Box.find({}, (error, data) => {
+        console.log(req, "/all")
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(data);
+        }
+    });
 });
+
+
+
 
 
