8603a90ac1e989e527a30d8269ef9c38788d1ddb
Brian GEARTY <geartybrian@gmail.com>
2020-11-21 17:23:13 -0500

    Added a router in App.js Added router, bootstrap, react bootstrap to package.json. Added a Nav component and put that inside of Header component. Added CartTable into Cart.js page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/Header.css"
+import Nav from "../components/Nav"
 
 function Header() {
   return (
+    
     <header className="header">
       <p>Header code will be here</p>
+      <div>
+      <Nav />
+      </div>
     </header>
   );
 }
