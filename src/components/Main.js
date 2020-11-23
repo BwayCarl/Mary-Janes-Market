ed6c5961447dec1acd7250f0c094d135336b521e
Carlos <bwaycarl@gmail.com>
2020-11-23 12:20:34 -0500

    Update AgeModal component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron"
 import ProductSection from "./ProductSection";
+import AgeModal from "../components/AgeModal"
 import "../styles/Main.css"
 
 function Main() {
   return (
     <main className="main">
         <p>Main section</p>
+      <AgeModal />
       <Jumbotron />
       <ProductSection />
     </main>
