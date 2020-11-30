6a8a91bb9d5ca3ee0cf3afa36a51f1493d0a767a
Brian GEARTY <geartybrian@gmail.com>
2020-11-30 09:27:13 -0500

    missing old files save point

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
-import "./App.css";
+import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Footer from "./components/Footer";
-import { UserProvider } from "./utils/contexts/userContext";
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Cart from "./pages/Cart"
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
     <div className="App">
-      <UserProvider>
         <Header />
         <Router>
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route exact path="/cart" component={Cart} />
+                </Switch>
         </Router>
         <Footer />
-      </UserProvider>
     </div>
   );
 }
 
 export default App;
+
+
+
