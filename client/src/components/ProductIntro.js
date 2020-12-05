88ac6bb4d53d4f8b60c94f8b680a5036d5619eec
Charles Metzger <cmetzjr@yahoo.com>
2020-12-05 01:10:34 -0500

    UI updates

--- client/src/components/ProductIntro.js
+++ client/src/components/ProductIntro.js
@@ -5,7 +5,7 @@ function ProductIntro() {
   return (
     <section className="ProductIntro mx-auto">
       <h2>Pre-Baked Boxes Delivered Monthly</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque hendrerit justo, eget faucibus felis vehicula sit amet. Curabitur bibendum vulputate auctor. Duis dictum augue nulla, ac ultrices lorem hendrerit a. Nullam placerat a lorem a laoreet. In in feugiat augue, et lacinia elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+      <p className="large-para pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque hendrerit justo, eget faucibus felis vehicula sit amet. Curabitur bibendum vulputate auctor. Duis dictum augue nulla, ac ultrices lorem hendrerit a. Nullam placerat a lorem a laoreet. </p>
     </section>
   );
 }
