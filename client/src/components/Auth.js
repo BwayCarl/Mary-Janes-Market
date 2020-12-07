404ca8071b7467efbdba44148b58dbfec305da75
Carlos <bwaycarl@gmail.com>
2020-12-07 12:25:06 -0500

    Clean up.

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -3,16 +3,22 @@ import { useHistory } from "react-router-dom";
 import { useUser } from "../utils/contexts/userContext";
 
 function Auth() {
-    const history = useHistory();
-    const [state] = useUser();
+  const history = useHistory();
+  const [state] = useUser();
   return (
-      <div  className="">
-        {state.user.displayName ? (
-          <p> {state.user.displayName} </p>
-        ) : (
-          <div className="mr-2" onClick={() => history.push("/login")}><button className="btn btn-login"><span className="login-icon"><i class="fas fa-user-lock"></i></span>Login</button></div>
+    <div className="">
+      {state.user.displayName ? (
+        <p> {state.user.displayName} </p>
+      ) : (
+          <div className="mr-2" onClick={() => history.push("/login")}>
+            <button className="btn btn-login">
+              <span className="login-icon">
+                <i class="fas fa-user-lock"></i>
+              </span>Login
+            </button>
+          </div>
         )}
-      </div>
+    </div>
 
   );
 }
