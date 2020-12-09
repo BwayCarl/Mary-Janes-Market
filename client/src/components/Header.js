4c32af85d423c13d24b444eac0312c73433c054e
Charles Metzger <cmetzjr@yahoo.com>
2020-12-09 01:24:59 -0500

    fix logo path

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
     <header className="header">
       <div className="row mx-1 justify-content-between align-items-center">
         <div className="">
-          <img className="logo" width="50px" src="./assets/Image/logoPic.png" alt="logo"></img>
+          <img className="logo" width="50px" src="../assets/Image/logoPic.png" alt="logo"></img>
           <span className="wordmark pl-3">Mary Jane's Market</span>
         </div>
           <Auth />
