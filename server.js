054d43bf766d33e5926d959afbc21779aeac28af
Brian GEARTY <geartybrian@gmail.com>
2020-12-09 21:19:29 -0500

    added dotenv to server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Cart = require("./models/cart.js");
 const express = require("express");
 const logger = require("morgan");
 const { resolve } = require("path");
+require('dotenv').config();
 
 const app = express();
 
