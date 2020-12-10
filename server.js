1c5010eae243d476d7b43ed42d12ffc4432463f6
Brian GEARTY <geartybrian@gmail.com>
2020-12-09 21:23:48 -0500

    commenting out on public on server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use(require("cors")());
 
